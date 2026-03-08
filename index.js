ea8f61809190f5b9b5bff6cb616f9758ace505b6
Lakshmi vangari <vangarilakshmi06@gmail.com>
2026-03-09 02:18:15 +0530

    Implement fetch for city temperature using OpenWeatherMap

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
+fetchData();
+async function fetchData(cityname){
+    try {
+        const cityName = cityname || document.getElementById("cityName").value.toLowerCase();
+        const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e1fc804d36e0cf73556d04ac118e0712&units=metric`);
+
+        if(!response.ok){
+            throw new Error("place not found");
+        }
+        const data = await response.json();
+        const temperature = data.main.temp;
+        const temperatureElement = document.getElementById("temperature");
+        temperatureElement.textContent = `Temperature at ${cityName}: ${Math.round(temperature)}°C`;
+        temperatureElement.style.display = "block"; 
+    }
+    catch(error){
+        console.error(error);
+        document.getElementById("temperature").textContent =
+        "Error fetching data. Please try again.";
+    }
+}
